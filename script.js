a40cbb93080fec9c9c629398f02734a3ebc480e2
Ayren Killman <ayrenkillman@gmail.com>
2024-04-12 15:36:32 +0000

    Added rebirth and balanced a lil bit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let clicks = 0;
 let clickBoost = 1;
 let autoClickers = 0;
+let rebirth = 0;
 //shop items
 const shopItems = [
     { name: "Small Click Boost", cost: 10, clickIncrease: 1 },
@@ -11,19 +12,28 @@ const shopItems = [
     { name: "Huge Click Boost", cost: 10000, clickIncrease: 50 },
     { name: "Massive Click Boost", cost: 500000, clickIncrease: 100 },
     { name: "Huge Click Boost", cost: 1000000, clickIncrease: 500 },
-    { name: "Cheap AutoClicker", cost: 200, clicksPerSecond: 1 },
-    { name: "Meh AutoClicker", cost: 500, clicksPerSecond: 3},
-    { name: "Ok AutoClicker", cost: 1000, clicksPerSecond: 10},
-    { name: "Good AutoClicker", cost: 5000, clicksPerSecond: 50},
-    { name: "Better AutoClicker", cost: 10000, clicksPerSecond: 100},
-    { name: "best AutoClicker", cost: 100000, clicksPerSecond: 1000},
-    { name: "Too much AutoClicker", cost: 1000000, clicksPerSecond: 3000},
-    { name: "Way too much AutoClicker", cost: 4000000, clicksPerSecond: 5000},
+    { name: "Cheap AutoClicker", cost: 1000, clicksPerSecond: 1 },
+    { name: "Meh AutoClicker", cost: 5000, clicksPerSecond: 3},
+    { name: "Ok AutoClicker", cost: 10000, clicksPerSecond: 10},
+    { name: "Good AutoClicker", cost: 50000, clicksPerSecond: 50},
+    { name: "Better AutoClicker", cost: 100000, clicksPerSecond: 100},
+    { name: "best AutoClicker", cost: 10000000, clicksPerSecond: 1000},
+    { name: "Too much AutoClicker", cost: 30000000, clicksPerSecond: 3000},
+    { name: "Way too much AutoClicker", cost: 50000000, clicksPerSecond: 5000},
+    { name: "2x clicks", cost: 10000000, multiplayclicks: 2},
+    { name: "3x clicks", cost: 80000000, multiplayclicks: 3},
+    { name: "4x clicks", cost: 6000000000, multiplayclicks: 4},
+    { name: "5x clicks", cost: 8000000000, multiplayclicks: 5},
+    { name: "Rebirth", cost: 200000000000000, Rebirth: 1},
+    
+
 ];
 //this updates the number on the html
 function updateClicks() {
     document.getElementById("clicks").textContent = clicks;
     document.getElementById("clickBoost").textContent = clickBoost;
+    document.getElementById("rebirth").textContent = rebirth;
+   
 }
 //this happens when the button is pressed
 function click() {
@@ -42,6 +52,15 @@ function buyItem(index) {
             autoClickers += item.clicksPerSecond;
             startAutoClickers();
         }
+        if (item.multiplayclicks) {
+            clickBoost *= item.multiplayclicks;
+        }
+        if (item.Rebirth){
+            rebirth += 1;
+            clickBoost = 1;
+            clicks = 1;
+            autoClickers = 0;
+        }
         updateClicks();
         displayShopItems();
     } else {
@@ -55,6 +74,9 @@ function startAutoClickers() {
         updateClicks();
     }, 1000);
 }
+
+
+
 //how you see the shop items in general
 function displayShopItems() {
     const itemsDiv = document.getElementById("items");
